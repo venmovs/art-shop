f5bc516bdbf4ee73c0595145bed4030d356b1ca2
vigen <vigen94@icloud.com>
2020-05-04 17:51:18 +0300

    added burger

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,7 @@ import calc from './modules/calc';
 import filter from "./modules/filter";
 import pictureSize from "./modules/pictureSize";
 import accordion from "./modules/accordion";
+import burger from "./modules/burger";
 
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -25,4 +26,5 @@ window.addEventListener('DOMContentLoaded', () => {
    filter();
    pictureSize('.sizes-block');
    accordion('.accordion-heading', '.accordion-block');
+   burger('.burger-menu', '.burger');
 });
