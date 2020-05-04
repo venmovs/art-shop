4b6c5d76fc8fdce7de91c778b46d6853f3e72ec9
vigen <vigen94@icloud.com>
2020-05-04 17:27:40 +0300

    added

---
+++ src/js/modules/accordion.js
@@ -0,0 +1,23 @@
+const accordion = (triggersSelector, itemSelector) => {
+    const btns = document.querySelectorAll(triggersSelector),
+        blocks = document.querySelectorAll(itemSelector);
+
+    blocks.forEach(block =>{
+        block.classList.add('animated', 'fadeInDown');
+    });
+
+
+
+    btns.forEach(btn => {
+       btn.addEventListener('click', function () {
+            if (!this.classList.contains('active')){
+                btns.forEach(btn =>{
+                   btn.classList.remove('active', 'active-style');
+                });
+                this.classList.add('active', 'active-style');
+            }
+       });
+    });
+};
+
+export default accordion;
